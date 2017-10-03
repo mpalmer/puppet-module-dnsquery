fabd186eed77c198173d418cababa762592e9399
Matt Palmer <mpalmer@hezmatt.org>
2017-10-03 19:43:32 +1100

    Add support for target:port returns from dns_srv

--- lib/puppet/parser/functions/dns_srv.rb
+++ lib/puppet/parser/functions/dns_srv.rb
@@ -1,10 +1,12 @@
 module Puppet::Parser::Functions
   newfunction(:dns_srv, :type => :rvalue, :doc => <<-EOS
-    Retrieves DNS SRV records and returns it as an array. Each record in the
-    array will be an array of [priority, weight, port, target] arrays.
-    Second argument is optional and can be either 'priority', 'weight', 'port'
-    or 'target', if supplied an array of only those elements is returned.
-    EOS
+    Retrieves DNS SRV records and returns it as an array.  Each record in
+    the array will be an array of [priority, weight, port, target] arrays.
+    Second argument is optional and can be either 'priority', 'weight',
+    'port', 'target', 'target:port'.  In that case, only the specified field
+    is returned; the special 'target:port' variant returns the target and
+    port joined with a colon.
+  EOS
   ) do |arguments|
     require 'resolv'
 
@@ -16,6 +18,8 @@ module Puppet::Parser::Functions
         [res.priority, res.weight, res.port, res.target.to_s]
       else
         case arguments[1]
+        when 'target:port'
+          "#{res.target.to_s}:#{res.port}"
         when 'priority', 'weight', 'port'
           res.send(arguments[1])
         when 'target'
